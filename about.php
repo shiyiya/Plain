328e364f6a56c3c0752f52b01029362fd91ce9df
ShiYi <3413343734@qq.com>
2018-05-30 10:53:19 +0800

    2018/5/30

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php 
+/**
+ * About 关于
+ * 
+ * @package custom
+ */
+$this->need('header.php'); ?>
+
+<div id="main" role="main">
+    <article class="post">
+        <h1 class="post-title" ><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
+        <p>
+        <?php Typecho_Widget::widget('Widget_Stat')->to($stat); ?>
+        我发表了<?php $stat->publishedPostsNum() ?>篇文章，有<?php $stat->publishedCommentsNum() ?>条评论。
+        </p>
+        <div class="post-content">
+            <?php  $this->content(); ?>
+        </div>
+    </article>
+    <?php $this->need('comments.php'); ?>
+</div>
+
+<?php $this->need('footer.php'); ?>
--- recent.php
+++ recent.php
@@ -1,7 +1,7 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php 
 /**
- * recent归档
+ * Recent归档
  * 
  * @package custom
  */
@@ -10,11 +10,17 @@ $this->need('header.php'); ?>
 <div id="main" role="main">
     <article class="post">
         <h1 class="post-title" ><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
+        <ul class="all-post">
+        <?php Typecho_Widget::widget('Widget_Stat')->to($stat); ?>
+        <li>文章总数：<?php $stat->publishedPostsNum() ?>篇</li>
+        <li>分类总数：<?php $stat->categoriesNum() ?>个</li>
+        <li>评论总数：<?php $stat->publishedCommentsNum() ?>条</li>
+        <li>页面总数：<?php $stat->publishedPagesNum() ?>个</li>
+        </ul>
         <div class="post-content">
             <?php $this->widget('Widget_Contents_Post_Recent','pageSize=10000')->parse('<li class="post-item">{year}-{month}-{day}:<a href="{permalink}">{title}</a></li>'); ?>
         </div>
     </article>
-    <?php $this->need('comments.php'); ?>
-</div><!-- end #main-->
+</div>
 
 <?php $this->need('footer.php'); ?>
