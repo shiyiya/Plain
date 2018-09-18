eae05c4a940f23ee56ccd8f9176191a0593a4d61
shiyi <shiyiya.11@gmail.com>
2018-09-18 13:44:40 +0800

    fixd： sw.js path error

--- footer.php
+++ footer.php
@@ -55,7 +55,7 @@
 <?php endif; ?>
 <script>
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('/sw.min.js')
         .then(function(reg){
             console.log('serviceWorker is working!');
         })
