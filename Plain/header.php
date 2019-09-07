05f7c93a92dfb366deec045ae672c5c25dd8921c
Shiyi <shiyiya.11@gmail.com>
2019-09-07 15:26:59 +0800

    [style]: update.

--- Plain/header.php
+++ Plain/header.php
@@ -9,16 +9,14 @@
     <meta name="renderer" content="webkit" />
 
     <title><?php $this->archiveTitle(array(
-            'category'  =>  _t('分类 %s 下的文章'),
-            'search'    =>  _t('包含关键字 %s 的文章'),
-            'tag'       =>  _t('标签 %s 下的文章'),
-            'author'    =>  _t('%s 发布的文章')
-        ), '', ' - '); ?><?php $this->options->title(); ?></title>
+                'category'  =>  _t('分类 %s 下的文章'),
+                'search'    =>  _t('包含关键字 %s 的文章'),
+                'tag'       =>  _t('标签 %s 下的文章'),
+                'author'    =>  _t('%s 发布的文章')
+            ), '', ' - '); ?><?php $this->options->title(); ?>
+    </title>
 
-    <meta name="author" content="<?php $this->options->title() ?>">
-    <meta name="keywords" content="<?php $this->keywords() ?>" />
-    <?php $this->header('keywords=&generator=&template=&pingback=&xmlrpc=&wlw=&commentReply=&rss1=&rss2=&atom='); ?>
-    
+    <?php $this->header('generator=&pingback=&xmlrpc=&wlw='); ?>
     <link rel="icon" href="<?php $this->options->fav() || $this->options->themeUrl('favicon.ico'); ?>" />
     <link rel="manifest" href="<?php $this->options->manifest('manifest.json') || $this->options->themeUrl('manifest.json') ?>" />
 
@@ -38,87 +36,86 @@
 
     <!-- Disable Baidu transformation -->
     <meta http-equiv="Cache-Control" content="no-transform " />
-    <meta http-equiv="Cache-Control" content="no-siteapp" /> 
+    <meta http-equiv="Cache-Control" content="no-siteapp" />
 
 
     <!-- Disable Baidu snapshot -->
     <meta name="Baiduspider" content="noarchive">
-   
+
 
     <!-- OGP https://www.ogp.me/ -->
-    <?php if($this->is('post')||$this->is('page')): ?>
-    <meta property="og:url" content="<?php $this->permalink() ?>" />
-    <meta property="og:type" content="blog" />
-    <meta property="og:title" content="<?php $this->title() ?>" />
-    <meta property="og:author" content="<?php $this->author(); ?>" />
-    <meta property="og:site_name" content="<?php $this->options->title() ?>" />
-    <meta property="og:description" content=" <?php $this->description() ?>" />
-    <meta property="og:locale:alternate" content="zh_CN" />
+    <?php if ($this->is('post') || $this->is('page')) : ?>
+        <meta property="og:url" content="<?php $this->permalink() ?>" />
+        <meta property="og:type" content="blog" />
+        <meta property="og:title" content="<?php $this->title() ?>" />
+        <meta property="og:author" content="<?php $this->author(); ?>" />
+        <meta property="og:site_name" content="<?php $this->options->title() ?>" />
+        <meta property="og:description" content=" <?php $this->description() ?>" />
+        <meta property="og:locale:alternate" content="zh_CN" />
     <?php endif; ?>
 
 
     <link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('style/plain.css'); ?>" />
-    
-   <!--  <link rel="preload" href="<?php $this->options->backGroundImage(); ?>" as="image"> -->
+
+    <!--  <link rel="preload" href="<?php $this->options->backGroundImage(); ?>" as="image"> -->
     <link rel="preload" href="https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js" as="script">
-    <link rel="preload" href="<?php $this->options->themeUrl('js/plain.js');?>" as="script">
+    <link rel="preload" href="<?php $this->options->themeUrl('js/plain.js'); ?>" as="script">
 
 
     <!-- Google analytics -->
     <?php _e($this->options->GoogleAnalytics) ?>
 
-    
+
     <!-- Used CDN -->
-	<script src="https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/jquery.pjax/1.9.6/jquery.pjax.min.js"></script>
 
 
     <!-- Custom Style -->
     <style>
         #root::before {
-            background-image: url(<?php $this->options->backGroundImage();
-            ?>)
+            background-image: url(<?php $this->options->backGroundImage(); ?>)
         }
     </style>
 
 </head>
 
 <body>
-<div id="root">
-    <div id='pjax'>
-    
-    <!-- Home does not need them -->
-    <?php if(!$this->is('index')) :?>
-        <link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('style/github-markdown.css'); ?>" />
-        <?php if(in_array('Prism', $this->options->effect)): ?>
-        <link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('prism/prism.css'); ?>" />
-        <?php endif; ?>
-    <?php endif;?>
+    <div id="root">
+        <div id='pjax'>
 
-    <header id="header">
-    <?php if($this->is('post')) :?>
-        <a id="logo" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a><?php _e(' /'); ?>
-        <h2 class="post-title"><?php $this->title() ?></h2>
-    <?php else:?>
-        <h1><a id="logo" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a></h1>
-        <p class="description"><?php $this->options->description() ?></p>
-        <nav id="nav-menu" role="navigation">
-            <a <?php if($this->is('index')): ?> class="current"<?php endif; ?> href="<?php $this->options->siteUrl(); ?>"><?php $this->options->home(); ?></a> 
-            <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
-            <?php while($pages->next()): ?>
-                <a <?php if($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
-            <?php endwhile; ?>
-        </nav>
-    <?php endif; ?>
-        <div class="link">
-            <?php if ($this->options->twitterLink): ?>
-                <a target="_blank" href="<?php $this->options->twitterLink(); ?>"><img alt="twitter-ico" src="<?php $this->options->themeUrl('./images/twitter.png'); ?>"></a>
+            <!-- Home does not need them -->
+            <?php if (!$this->is('index')) : ?>
+                <link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('style/github-markdown.css'); ?>" />
+                <?php if (in_array('Prism', $this->options->effect)) : ?>
+                    <link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('prism/prism.css'); ?>" />
+                <?php endif; ?>
             <?php endif; ?>
-            <?php if ($this->options->GitHubLink): ?>
-                <a target="_blank" href="<?php $this->options->GitHubLink(); ?>"><img alt="GitHub-ico" src="<?php $this->options->themeUrl('./images/github.png'); ?>"></a>
-            <?php endif; ?>
-            <?php if ($this->options->rssLink): ?>
-                <a target="_blank" href="<?php $this->options->rssLink(); ?>"><img alt="rss-ico" src="<?php $this->options->themeUrl('./images/rss.png'); ?>"></a>
-            <?php endif; ?>
-        </div>
-    </header>
+
+            <header id="header">
+                <?php if ($this->is('post')) : ?>
+                    <a id="logo" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a><?php _e(' /'); ?>
+                    <h2 class="post-title"><?php $this->title() ?></h2>
+                <?php else : ?>
+                    <h1><a id="logo" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a></h1>
+                    <p class="description"><?php $this->options->description() ?></p>
+                    <nav id="nav-menu" role="navigation">
+                        <a <?php if ($this->is('index')) : ?> class="current" <?php endif; ?> href="<?php $this->options->siteUrl(); ?>"><?php $this->options->home(); ?></a>
+                        <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+                        <?php while ($pages->next()) : ?>
+                            <a <?php if ($this->is('page', $pages->slug)) : ?> class="current" <?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
+                        <?php endwhile; ?>
+                    </nav>
+                <?php endif; ?>
+                <div class="link">
+                    <?php if ($this->options->twitterLink) : ?>
+                        <a target="_blank" href="<?php $this->options->twitterLink(); ?>"><img alt="twitter-ico" src="<?php $this->options->themeUrl('./images/twitter.png'); ?>"></a>
+                    <?php endif; ?>
+                    <?php if ($this->options->GitHubLink) : ?>
+                        <a target="_blank" href="<?php $this->options->GitHubLink(); ?>"><img alt="GitHub-ico" src="<?php $this->options->themeUrl('./images/github.png'); ?>"></a>
+                    <?php endif; ?>
+                    <?php if ($this->options->rssLink) : ?>
+                        <a target="_blank" href="<?php $this->options->rssLink(); ?>"><img alt="rss-ico" src="<?php $this->options->themeUrl('./images/rss.png'); ?>"></a>
+                    <?php endif; ?>
+                </div>
+            </header>
--- Plain/index.php
+++ Plain/index.php
@@ -1,34 +1,42 @@
 <?php
+
 /**
  * 思于心现于形，极简主题，专注于创作。
- * 
+ *
  * @package Plain
  * @author  ShiYi,ikirby
- * 
- * @version 1.2.3
+ *
+ * @version 1.2.4
  * @link https://www.runtua.cn/
  * @link https://ikirby.me/
  */
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
- $this->need('header.php');
- ?>
+$this->need('header.php');
+?>
+
+<style>
+	#pjax {
+		margin: 120px auto;
+	}
+</style>
+
 <div id="main" role="main">
 	<div class="content">
 		<article class="post">
 			<ul class="post-list">
-				<?php while($this->next()): ?>
-				<section class="post-item">
-					<a href="<?php $this->permalink(); ?>" title="<?php $this->title(); ?>">
-						<?php $this->title() ?>
-					</a>
-					<span class="post-meta">
-						<?php _e('/'); ?>
-						<time datetime="<?php $this->date('c'); ?>">
-							<?php $this->date(); ?>
-						</time>
-					</span>
-				</section>
+				<?php while ($this->next()) : ?>
+					<section class="post-item">
+						<a href="<?php $this->permalink(); ?>" title="<?php $this->title(); ?>">
+							<?php $this->title() ?>
+						</a>
+						<span class="post-meta">
+							<?php _e('/'); ?>
+							<time datetime="<?php $this->date('c'); ?>">
+								<?php $this->date(); ?>
+							</time>
+						</span>
+					</section>
 				<?php endwhile; ?>
 			</ul>
 		</article>
